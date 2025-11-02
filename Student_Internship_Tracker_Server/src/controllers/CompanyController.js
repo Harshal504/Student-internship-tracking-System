ab329b72f9a9dd5831c104448c760951165b1fef
Vaishakh Malode <vaishakhmalode98@gmail.com>
2025-11-02 17:23:57 +0530

    Implement CRUD operations for companies

--- Student_Internship_Tracker_Server/src/controllers/CompanyController.js
+++ Student_Internship_Tracker_Server/src/controllers/CompanyController.js
@@ -13,3 +13,55 @@ export async function getAllCompanies(request, response) {
         response.status(500).send({ message: 'Something went wrong' });
     }
 }
+export async function addCompanies(request, response) {
+    try {
+        const conn = getConnectionObject();
+        const data = request.body
+        const qry = `INSERT into COMPANY(name, email, password, tech_domain) VALUES ('${data.name}', '${data.email}', '${data.password}', '${data.tech_domain}')`;
+        const [resultSet] = await conn.query(qry);
+        if (resultSet.affectedRows === 1) {
+            response.status(200).send({ message: 'Company registered successfully' });
+        }
+        else {
+            response.status(500).send({ message: 'Company registration failed' });
+        }
+    } catch (error) {
+        console.log(error);
+        response.status(500).send({ message: 'Something went wrong' });
+    }
+}
+
+export async function updateCompanies(request, response) {
+    try {
+        const conn = getConnectionObject();
+        const { name, email, tech_domain } = request.body;
+        const qry = `UPDATE company SET name='${name}', email='${email}', tech_domain='${tech_domain}' WHERE company_id= ?`;
+        const [resultSet] = await conn.query(qry, [request.params.id]);
+        if (resultSet.affectedRows === 1) {
+            response.status(200).send({ message: 'Company Updated' });
+        }
+        else {
+            response.status(500).send({ message: 'Company update operation failed' });
+        }
+    } catch (error) {
+        console.log(error);
+        response.status(500).send({ message: 'Something went wrong' });
+    }
+}
+
+export async function delCompanies(request, response) {
+    try {
+        const conn = getConnectionObject();
+        const qry = `DELETE FROM company WHERE company_id= ?`;
+        const [resultSet] = await conn.query(qry, [request.params.id]);
+        if (resultSet.affectedRows === 1) {
+            response.status(200).send({ message: 'Company Deleted' });
+        }
+        else {
+            response.status(500).send({ message: 'Company delete operation failed' });
+        }
+    } catch (error) {
+        console.log(error);
+        response.status(500).send({ message: 'Something went wrong' });
+    }
+}
