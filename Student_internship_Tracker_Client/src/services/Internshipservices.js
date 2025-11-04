75b36d58165c7cc8c9ed40db71c2c2001a2007e5
Vaishakh Malode <vaishakhmalode98@gmail.com>
2025-11-04 22:38:28 +0530

    Refactor Internship services to add new functionalities
    Removed authentication header from getAllInternships function and added new functions for posting internships, applying to internships, and updating internship status.

--- Student_internship_Tracker_Client/src/services/Internshipservices.js
+++ Student_internship_Tracker_Client/src/services/Internshipservices.js
@@ -1,8 +1,18 @@
 import axios from "axios";
-import { getAuthHeader } from "./TokenService";
-
-
-
 export function getAllInternships() {
-    return axios.get("http://localhost:4400/internship/data", getAuthHeader());
+    return axios.get("http://localhost:4400/internship/data");
 }
+
+export function postInternship(data) {
+    return axios.post(`http://localhost:4400/internship/data`, data);
+}
+
+export function applyToInternship(data) {
+    return axios.post(`http://localhost:4400/application/create`, data);
+}
+
+export const updateInternshipStatus = async (internshipId, newStatus) => {
+  return axios.put(`http://localhost:4400/status/${internshipId}`, {
+    status: newStatus,
+  });
+};
